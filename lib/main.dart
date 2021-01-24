19cebfc35541d01144b55633f48fd15e4e3e176b
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-01-24 04:01:28 -0500

    Almost finished my Xylophone

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData.dark(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          // primarySwatch: Colors.blue,
+          // primaryColor: Colors.black,
+          // backgroundColor: Colors.black,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          // visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+      home: MyHomePage(title: 'Xylophone'),
     );
   }
 }
@@ -53,45 +55,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  // Wrong assignment of notes:
-  // Map<String, int> musicalNotes = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7};
-  // Map<String, int> musicalNotes = {'C': 1, 'D': 2, 'E': 3, 'F': 4, 'G': 5, 'A': 6, 'B': 7};
-  // Map<String, int> musicalNotes = {'B': 1, 'A': 2, 'G': 3, 'F': 4, 'E': 5, 'D': 6, 'C': 7};
-
+  // Notes assignment:
   Map<String, int> musicalNotes = {'C': 1, 'D': 2, 'E': 3, 'F': 4, 'G': 5, 'A': 6, 'B': 7};
 
-  List<String> twinkleTwinkleMusicSheet = [null, null, 'C', 'C', 'G', 'G', 'A', 'A', 'G', null, 'F', 'F', 'E', 'E', 'D', 'D', 'C'];
+  // The Twinkle Twinkle Music Sheet
+  List<String> twinkleTwinkleMusicSheet = [null, 'C', 'C', 'G', 'G', 'A', 'A', 'G', null, 'F', 'F', 'E', 'E', 'D', 'D', 'C'];
 
   // add it to your class as a static member
   // static AudioCache player = AudioCache();
   // or as a local variable
   // final player = AudioCache();
-
   // You can optionally pass a prefix to the constructor if all of your audios are in a specific folder inside the assets folder.
   AudioCache player = AudioCache(prefix: 'assets/audio/');
 
-  void _incrementCounter() {
-    // call this method when desired
-    // player.play('note1.wav');
-    // playNote('A');
-    playTwinkleTwinkle();
-
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   void playTwinkleTwinkle() {
     // Preloads all the audio files (avoids the initial delay):
-    // preloadsTheAudioFiles();
+    preloadsTheAudioFiles();
 
-    final countDownTimer = CountdownTimer(Duration(milliseconds: 8000), Duration(milliseconds: 500));
+    final countDownTimer = CountdownTimer(Duration(milliseconds: 7500), Duration(milliseconds: 500));
     countDownTimer.listen((data) {
       int index = countDownTimer.elapsed.inMilliseconds ~/ 500.0;
       // print(index);
@@ -154,22 +135,158 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                // margin: EdgeInsets.symmetric(horizontal: 20.0),
+                margin: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                child: FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    playNote('C');
+                  },
+                  child: Text(
+                    'C',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                // margin: EdgeInsets.symmetric(horizontal: 30.0),
+                margin: EdgeInsets.fromLTRB(30, 5, 30, 5),
+                child: FlatButton(
+                  color: Colors.orange,
+                  onPressed: () {
+                    playNote('D');
+                  },
+                  child: Text(
+                    'D',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                // margin: EdgeInsets.symmetric(horizontal: 40.0),
+                margin: EdgeInsets.fromLTRB(40, 5, 40, 5),
+                child: FlatButton(
+                  color: Colors.yellow.shade600,
+                  onPressed: () {
+                    playNote('E');
+                  },
+                  child: Text(
+                    'E',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                // margin: EdgeInsets.symmetric(horizontal: 50.0),
+                margin: EdgeInsets.fromLTRB(50, 5, 50, 5),
+                child: FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    playNote('F');
+                  },
+                  child: Text(
+                    'F',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                // margin: EdgeInsets.symmetric(horizontal: 60.0),
+                margin: EdgeInsets.fromLTRB(60, 5, 60, 5),
+                child: FlatButton(
+                  color: Colors.purple,
+                  onPressed: () {
+                    playNote('G');
+                  },
+                  child: Text(
+                    'G',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                // margin: EdgeInsets.symmetric(horizontal: 70.0),
+                margin: EdgeInsets.fromLTRB(70, 5, 70, 5),
+                child: FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    playNote('A');
+                  },
+                  child: Text(
+                    'A',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                // margin: EdgeInsets.symmetric(horizontal: 80.0),
+                margin: EdgeInsets.fromLTRB(80, 5, 80, 5),
+                child: FlatButton(
+                  color: Colors.purpleAccent,
+                  onPressed: () {
+                    playNote('B');
+                  },
+                  child: Text(
+                    'B',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 25.0,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        onPressed: playTwinkleTwinkle,
+        tooltip: 'Twinkle',
+        child: Icon(Icons.play_arrow),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
